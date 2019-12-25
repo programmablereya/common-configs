50ce400b17d00531152f7ef8cd501f1c75ae13b3
Mari <mstaib.git@reya.zone>
2019-12-25 14:35:00 -0500

    Use HTTPS for new-host checkout.

--- new-host.sh
+++ new-host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ! -d ~/.common-configs ]]; then
-  git clone git@github.com:programmablereya/common-configs.git ~/.common-configs
+  git clone https://github.com/programmablereya/common-configs.git ~/.common-configs
   cd ~/.common-configs
 else
   cd ~/.common-configs
