ae557a03bcf4e0c5dcf540e53b89c76aec7987e1
Mari <mstaib.git@reya.zone>
2020-06-26 19:46:35 -0400

    Update new-host.sh

--- new-host.sh
+++ new-host.sh
@@ -29,7 +29,7 @@ fi
 function getCurrentGithubId() {
   local RESULT;
   RESULT=$(ssh -o "IdentitiesOnly=yes" -i ~/.ssh/id_ed25519 -T git@github.com </dev/null 2>&1) 
-  [[ $? -ne 255 ]] || echo "$RESULT" >&2; exit 1
+  [[ $? -ne 255 ]] || { echo "$RESULT" >&2; exit 1; }
   USERNAME=$(echo "$RESULT" | sed -n 's/^Hi \([^!]\+\)! You'"'"'ve successfully authenticated, but GitHub does not provide shell access\.$/Successfully authenticated to GitHub as \1/; T; p') || exit 1
   echo "$USERNAME"
   [[ -n "$USERNAME" ]];
